9cacf1f4ba156e491cdb80fa8f290f7d8b53befc
echt16 <albertivanov375@gmail.com>
2024-04-07 02:00:06 +0300

    Last commit

---
+++ homework1part1/Pages/Countries.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace homework1part1.Pages
+{
+    public class CountriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
